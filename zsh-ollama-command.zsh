be176a2125805536bdfb717eaa890331812cd442
Parker Gabel <parker.d.gabel@gmail.com>
2025-04-09 16:08:15 -0700

    Changed prompt

--- zsh-ollama-command.zsh
+++ zsh-ollama-command.zsh
@@ -81,6 +81,8 @@ fzf_ollama_commands() {
     -d "$ZSH_OLLAMA_COMMANDS_REQUEST_BODY")
   local ret=$?
 
+  echo $ZSH_OLLAMA_COMMANDS_RESPONSE
+
   # trim response content newline
   ZSH_OLLAMA_COMMANDS_SUGGESTION=$(echo $ZSH_OLLAMA_COMMANDS_RESPONSE | tr -d '\n\r' | tr -d '\0' | jq '.')
   check_status
